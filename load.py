c2ae0fb70af32842f938bfff2c9b791ef1c2293e
topschrott <topschrott@me.com>
2025-09-27 21:42:20 +0100

    Optimise route.

--- load.py
+++ load.py
@@ -2,6 +2,7 @@
 import _thread
 import logging
 import os
+import math
 import tkinter as tk
 from datetime import datetime, timezone, timedelta
 from tkinter import ttk
@@ -38,7 +39,10 @@ def _ebgs_fetch(path, params):
 
 def _ebgs_fetch_factions(faction_name):
     """ Fetch faction information for given faction name. """
-    return _ebgs_fetch('factions', {'name': faction_name})
+    return _ebgs_fetch('factions', {
+        'name': faction_name,
+        'systemDetails': 'true'
+    })
 
 
 class _PluginConfigs(Enum):
@@ -57,10 +61,11 @@ class _PluginConfigs(Enum):
 
 class _FactionPresence:
 
-    def __init__(self, system_name, updated_at):
+    def __init__(self, system_name, updated_at, location):
         self.system_name = system_name
         self.updated_at = datetime.fromisoformat(updated_at)
         self.age = datetime.now(timezone.utc) - self.updated_at
+        (self.x, self.y, self.z) = location
 
     @property
     def nice_age(self):
@@ -75,6 +80,13 @@ class _FactionPresence:
             return f'{self.age.total_seconds() // minute} minutes'
         return f'{self.age.total_seconds()}s'
 
+    def distance_to(self, target):
+        """ Return distance to target. """
+        return math.sqrt(
+            (target.x - self.x) ** 2 +
+            (target.y - self.y) ** 2 +
+            (target.z - self.z) ** 2)
+
 
 class _PluginPrefs:
     """ Plugin preferences. """
@@ -83,6 +95,7 @@ class _PluginPrefs:
         self.__faction_name_var = tk.StringVar(value=_PluginConfigs.FACTION_NAME.get_str())
         self.__text_frame = None
         self.systems = []
+        self.start_system = None
 
     def create_frame(self, parent: nb.Notebook):
         """ Create and return preferences frame. """
@@ -126,9 +139,15 @@ class _PluginPrefs:
             faction_systems = []
             for faction in _ebgs_fetch_factions(faction_name):
                 for presence in faction['faction_presence']:
+                    sys_details = presence['system_details']
                     faction_systems.append(_FactionPresence(
                         presence['system_name'],
-                        presence['updated_at']))
+                        presence['updated_at'],
+                        (sys_details['x'], sys_details['y'], sys_details['z'])))
+
+            # Optimise route for less jumps
+            start_system = self.start_system or faction_systems[0]
+            faction_systems = self.__optimise_route(start_system, faction_systems)
 
             self.__text_frame.after_idle(self.__faction_systems_received, faction_systems)
 
@@ -138,6 +157,21 @@ class _PluginPrefs:
                 tk.messagebox.showerror,
                 'Download error',
                 f'Error downloading faction systems: {e}.')
+
+    def __optimise_route(self, start_system, systems):
+        """ Optimise list of systems for fewer jumps. """
+        systems = list(systems)
+        route = [start_system]
+        while systems:
+            # Find nearest
+            cur_system = route[-1]
+            closest_system = min(systems, key=cur_system.distance_to)
+            # Remove system from list and add to route
+            systems.remove(closest_system)
+            route.append(closest_system)
+
+        # Return route without start system
+        return route[1:]
 
     def __faction_systems_received(self, faction_systems):
         """ List of systems loaded. """
@@ -187,8 +221,8 @@ class _PluginApp:
 
     def on_journal_entry(self, system):
         """ New journal entry. """
-        if self.__systems and system == self.__systems[0]:
-            self.__systems.pop(0)
+        if self.__systems and system in self.__systems:
+            self.__systems.remove(system)
             self.__update_next_system()
 
     def __on_skip(self, _event):
@@ -237,9 +271,14 @@ def journal_entry(
     _is_beta: bool,
     system: str,
     _station: str,
-    _entry: Dict[str, Any],
+    entry: Dict[str, Any],
     _state: Dict[str, Any]
 ) -> Optional[str]:
     """ Called by EDMC for every new journal entry. """
     if system:
+        if 'StarSystem' in entry and 'StarPos' in entry:
+            timestamp = entry['timestamp']
+            star_system = entry['StarSystem']
+            location = entry['StarPos']
+            _plugin_prefs.start_system = _FactionPresence(star_system, timestamp, location)
         _plugin_app.on_journal_entry(system)
